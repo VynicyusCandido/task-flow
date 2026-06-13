e9fa508e6144781ffbe8f438b288fe8fdcafe400
VynicyusCandido <vynicyuscandido@gmail.com>
2026-06-13 17:17:52 -0300

    erro de middleware

--- taskflow-app/src/app/services/token.ts
+++ taskflow-app/src/app/services/token.ts
@@ -1,7 +1,5 @@
 import Introspect from "@/@types/Introspect";
 import { jwtDecode } from "jwt-decode";
-import Cookies from "js-cookie";
-import { Auth } from "../enums";
 
 export default async function Token(token: string): Promise<Introspect> {
   try {
@@ -9,7 +7,6 @@ export default async function Token(token: string): Promise<Introspect> {
     const isExpired = decoded.exp ? decoded.exp * 1000 < Date.now() : true;
 
     if (isExpired) {
-      Cookies.remove(Auth.AUTH_TOKEN);
       return { active: false };
     }
 
--- taskflow-app/src/proxy.ts
+++ taskflow-app/src/proxy.ts
@@ -5,13 +5,22 @@ import Introspect from '@/@types/Introspect';
 
 export async function proxy(request: NextRequest) {
   const token = request.cookies.get(Auth.AUTH_TOKEN)?.value;
+  
   if (token) {
     const introspect: Introspect = await Token(token);
     if (introspect?.active) {
       return NextResponse.next();
     }
   }
-  return NextResponse.redirect(new URL('/login', request.url));
+
+  // Se o token for inválido ou não existir, redireciona para login e limpa o cookie
+  const response = NextResponse.redirect(new URL('/login', request.url));
+  
+  if (token) {
+    response.cookies.delete(Auth.AUTH_TOKEN);
+  }
+  
+  return response;
 }
 
 export default async function middleware(request: NextRequest) {
